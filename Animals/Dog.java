366ea7e511b7e2d2f865b5c26a4b49b18e4480a5
Sasha <yukka.yu@gmail.com>
2022-08-11 14:00:09 +0300

    Dog corrected

--- Animals/Dog.java
+++ Animals/Dog.java
@@ -1,24 +1,11 @@
 package Animals;
-public class Dog {
-    int height;
-    int weight;
-    String eyeColor;
-    String name;
-    String breed;
-    Boolean vaccinated;
-    String color;
-    String dateOfBirth;
+public class Dog extends HomeAnimal{
+    Boolean trained;
 
     /**Конструктор */
-    public Dog(int height, int weight, String eyeColor, String name, String breed, Boolean vaccinated, String color, String dateOfBirth){
-        this.height = height;
-        this.weight = weight;
-        this.eyeColor = eyeColor;
-        this.name = name;
-        this.breed = breed;
-        this.vaccinated = vaccinated;
-        this.color = color;
-        this.dateOfBirth = dateOfBirth;
+    public Dog(int height, int weight, String eyeColor, String name, String breed, Boolean vaccinated, String color, String dateOfBirth, Boolean trained){
+        super(height, weight, eyeColor, name, breed, vaccinated, color, dateOfBirth); 
+        this.trained = trained;
     }
 
     public void makeSound(){
@@ -27,6 +14,15 @@ public class Dog {
     
     public void love(){
         System.out.println("I love you");
+    }
+
+    @Override
+    public String printInfo(){
+        return String.format("%s\ntrained: ", super.printInfo(), this.trained);
+    }
+
+    public void training(){
+        trained = true;
     }
 
 }
