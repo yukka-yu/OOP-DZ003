03e62840ef27f1a9f284c6029a86c0187e7f2c89
Bobby <mif210210@gmail.com>
2022-08-11 11:26:28 +0300

    edit cat

--- Cat.java
+++ Cat.java
@@ -8,6 +8,18 @@ public class Cat {
     private String color;
     private String dateOfBith;
 
+    public Cat(int height, int weight, String eyesColor, String name,
+               String breed, boolean vaccinated, String color, String dateOfBith){
+        this.height = height;
+        this.weight = weight;
+        this.eyesColor = eyesColor;
+        this.name = name;
+        this.breed = breed;
+        this.vaccinated = vaccinated;
+        this.color = color;
+        this.dateOfBith = dateOfBith;
+    }
+
 
     public void makeSound(){
         System.out.println("Miiiayyyy");
