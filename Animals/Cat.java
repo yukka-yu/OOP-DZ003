f926df03cd6a7a9f5f74d8973115410a00240fce
Bobby <mif210210@gmail.com>
2022-08-11 13:37:15 +0300

    edit cat

--- Animals/Cat.java
+++ Animals/Cat.java
@@ -1,33 +1,19 @@
 package Animals;
-public class Cat {
-    private int height;
-    private int weight;
-    private String eyesColor;
-    private String name;
-    private String breed;
-    private boolean vaccinated;
-    private String color;
-    private String dateOfBith;
-
-    public Cat(int height, int weight, String eyesColor, String name,
-               String breed, boolean vaccinated, String color, String dateOfBith){
-        this.height = height;
-        this.weight = weight;
-        this.eyesColor = eyesColor;
-        this.name = name;
-        this.breed = breed;
-        this.vaccinated = vaccinated;
-        this.color = color;
-        this.dateOfBith = dateOfBith;
+public class Cat extends HomeAnimal {
+    public Cat(int height, int weight, String eyesColor, String color, String dateOfBirth, Boolean vaccinated, String name, String breed) {
+        super(height, weight, eyesColor, color, dateOfBirth, vaccinated, name, breed);
     }
 
-
+    @Override
     public void makeSound(){
         System.out.println("Miiiayyyy");
     }
 
-    public void love(){
-        System.out.println("Tenderness");
-    }
 
+
+    @Override
+    public void love() {
+        System.out.println("I love you, miiiayyy");;
+    }
 }
+
