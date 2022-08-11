cda0851a553bcca1519ea850f8d81b279be58d33
Sasha <yukka.yu@gmail.com>
2022-08-11 11:26:11 +0300

    new commit with Dog branch

---
+++ Dog.java
@@ -0,0 +1,33 @@
+package sem003;
+
+public class Dog {
+    int height;
+    int weight;
+    String eyeColor;
+    String name;
+    String breed;
+    Boolean vaccinated;
+    String color;
+    String dateOfBirth;
+
+    /**Конструктор */
+    public Dog(int height, int weight, String eyeColor, String name, String breed, Boolean vaccinated, String color, String dateOfBirth){
+        this.height;
+        this.weight;
+        this.eyeColor;
+        this.name;
+        this.breed;
+        this.vaccinated;
+        this.color;
+        this.dateOfBirth;
+    }
+
+    public void makeSound(){
+        System.out.println("Woof!");
+    }
+    
+    public void love(){
+        System.out.println("I love you");
+    }
+
+}
