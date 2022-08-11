f6b05174342ae259dd9eecb612445527db6b8188
Yulia Batrakova <julichka_71@mail.ru>
2022-08-11 11:01:32 +0300

    added tiger

---
+++ Tiger.java
@@ -0,0 +1,17 @@
+// III. Реализовать класс тигр, содержащий слелующие поля: Юлия
+// 1. Рост животного
+// 2. Вес животного
+// 3. Цвет глаз животного
+// 4. Место обитания
+// 5. Дата нахождения
+// И методами:
+// 1. Издавать звук
+
+public class Tiger extends Animals {
+    public Tiger (int height, int weight, String eyesColor, String liveArea, int dateOfFound){
+        super(height, weight, eyesColor, liveArea, dateOfFound);
+    }
+
+    
+    
+}
