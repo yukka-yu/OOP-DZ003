df7cc56ee2e799e9ff0b2237f44bf8e049954605
Bobby <mif210210@gmail.com>
2022-08-11 11:21:13 +0300

    add cat

---
+++ Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    private int height;
+    private int weight;
+    private String eyesColor;
+    private String name;
+    private String breed;
+    private boolean vaccinated;
+    private String color;
+    private String dateOfBith;
+
+
+    public void makeSound(){
+        System.out.println("Miiiayyyy");
+    }
+
+    public void love(){
+        System.out.println("Tenderness");
+    }
+
+}
