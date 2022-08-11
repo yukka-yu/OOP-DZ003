dce3c1ba6564b181653eed19d3018af92b31771e
Inna <shvartz_i@mail.ru>
2022-08-11 11:07:51 +0300

    chicken added

---
+++ Chicken.java
@@ -0,0 +1,20 @@
+﻿public class Chicken {
+
+    private double height;
+    private double weight;
+    private double heightOfFly;
+    private String eyeColor;
+
+    public Chicken (double height, double weight, double heightOfFly, String eyeColor) {
+        
+        this.height = height;
+        this.weight = height;
+        this.height = heightOfFly;
+        this.eyeColor = eyeColor;
+
+    }
+
+    public String toFly() {
+        return String.format("%s %4.2f", "лечу на бреющем полете, высота ",this.heightOfFly);
+    }
+}
